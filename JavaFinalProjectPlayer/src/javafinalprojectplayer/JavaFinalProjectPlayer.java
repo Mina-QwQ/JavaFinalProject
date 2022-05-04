5db473f5a67d05b6b904fcb7de05b2ebf01c380d
MeiNa Xie <meinaxie@10-18-218-36.dynapool.wireless.nyu.edu>
2022-05-04 19:09:57 -0400

    start game interface

--- JavaFinalProjectPlayer/src/javafinalprojectplayer/JavaFinalProjectPlayer.java
+++ JavaFinalProjectPlayer/src/javafinalprojectplayer/JavaFinalProjectPlayer.java
@@ -15,7 +15,6 @@ import java.net.Socket;
 import java.util.Scanner;
 import java.awt.*;
 import java.awt.event.*;
-import static javafinalprojectplayer.JavaFinalProjectPlayer.jf;
 import javax.swing.*;
 
 /**
@@ -27,9 +26,10 @@ public class JavaFinalProjectPlayer  {
     /**
      * @param args the command line arguments
      */
-    static JFrame jf = new JFrame("Messages");
     
     public static void main(String[] args) {
+        new StartGame();
+        /*
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         
         System.out.print("Enter Server: ");
@@ -60,6 +60,7 @@ public class JavaFinalProjectPlayer  {
         JButton startButton = new JButton("Start Game");
         startButton.addActionListener(new StartGame());
         jf.add(startButton);
+*/
         
     }
 
@@ -70,50 +71,68 @@ public class JavaFinalProjectPlayer  {
 class StartGame implements ActionListener {
     
     JButton[] bunnies = new JButton[2]; //holds Boolean True if bunny shows up
-    
+    int score = 0;
+    int timer = 0;
     StartGame() {
-        //**********LAYOUT for GAME***********************
-        //JFrame BorderLayout 
-        // JPanel of score NORTHEAST
-        //JPanel of timer on NORTHWEST
-        //JPanel of bunnies CENTER, GridLayout 1 X 2 
-        //JText for client guess SOUTH 
-        //Submit Button SOUTHEAST
-        
         
     }
     @Override
         public void actionPerformed(ActionEvent arg0){
-            
+            //**********LAYOUT for GAME***********************
+            //JFrame BorderLayout 
+            JFrame startgame = new JFrame("Pick your bunnies");
+            startgame.setLayout(new BorderLayout());
+            startgame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            startgame.setSize(500, 400);
+
+            // JPanel of score and timer NORTHEAST and NORTHWEST
+            JPanel scoretimer = new JPanel();
+            scoretimer.setLayout(new BorderLayout());
+            scoretimer.setSize(500, 100);
+            startgame.add(scoretimer, BorderLayout.NORTH);
+
+            JLabel scorel = new JLabel(" "+score+"");
+            scorel.setFont(new Font("Serif", Font.BOLD, 50));
+            scorel.setForeground(Color.PINK);
+            scorel.setBackground(Color.gray);
+            scorel.setOpaque(true);
+            scoretimer.add(scorel, BorderLayout.WEST);
+
+            JLabel timerl = new JLabel(" "+timer+" ");
+            timerl.setFont(new Font("Serif", Font.BOLD, 50));
+            timerl.setForeground(Color.PINK);
+            timerl.setBackground(Color.gray);
+            timerl.setOpaque(true);
+            scoretimer.add(timerl, BorderLayout.EAST);
+
+            //JPanel of bunnies CENTER, GridLayout 1 X 2 
+            JPanel bunniesp = new JPanel();
+            bunniesp.setSize(500, 200);
+            bunniesp.setLayout(new GridLayout(1, 2));
+            startgame.add(bunniesp, BorderLayout.CENTER);
+
+            JButton bunny1 = new JButton("1");
+            bunniesp.add(bunny1);
+            bunnies[0] = bunny1;
+            JButton bunny2 = new JButton("2");
+            bunnies[1] = bunny2;
+            bunniesp.add(bunny2);
+
+            //JText for client guess SOUTH 
+            JPanel guessp = new JPanel();
+            guessp.setSize(500, 100);
+            startgame.add(guessp, BorderLayout.SOUTH);
+            JTextField guessbox= new JTextField(20);
+            guessp.add(guessbox);
+            //Submit Button SOUTHEAST
+            JButton submit = new JButton("Submit");
+            submit.addActionListener(new SubmitGuess());
+            guessp.add(submit);
+
+            startgame.setVisible(true);
             
         }
 
-    void won(){ //display won message to client 
-        JLabel label = new JLabel("You Won!!!", SwingConstants.CENTER);
-        label.setFont(new Font("Serif", Font.BOLD, 20));
-        label.setForeground(Color.RED);
-        label.setBackground(Color.ORANGE);
-        label.setOpaque(true);
-
-        jf.add(label); 
-        //frame.setSize(300,150);
-        jf.setVisible(true);
-    }
-    void lose(){ //display lsot message to client
-        JLabel label = new JLabel("You Lost...", SwingConstants.CENTER);
-        label.setFont(new Font("Serif", Font.BOLD, 20));
-        label.setForeground(Color.RED);
-        label.setBackground(Color.ORANGE);
-        label.setOpaque(true);
-
-        jf.add(label); 
-        //frame.setSize(300,150);
-        jf.setVisible(true);
-    }
-    void tie(){
-        //text.setText("");
-        // reset buttons/bunnies
-    }
 }
 
 
@@ -123,11 +142,6 @@ class Bunny implements ActionListener {
     //changes boolean true false, change icon Image with/wout bunny
     @Override
         public void actionPerformed(ActionEvent arg0){
-            //**********LAYOUT for GAME***********************
-            //JFrame BorderLayout 
-            // JPanel of opponent's guess NORTH
-            //JPanel of StartButton  SOUTH
-            //JPanel of bunnies CENTER, GridLayout 1 X 2 oppenent's guess on left, your guess on right
             
             return;
         }
@@ -140,69 +154,50 @@ class SubmitGuess implements ActionListener {
     @Override
         public void actionPerformed(ActionEvent arg0) {
             
+            //**********LAYOUT for GAME***********************
+            //JFrame BorderLayout 
+            // JPanel of opponent's guess NORTH
+            //JPanel of StartButton  SOUTH
+            //JPanel of bunnies CENTER, GridLayout 1 X 2 oppenent's guess on left, your guess on right
+            //start again button
             
             return;
         }
 }
-        
-        
-class UserInput extends Thread{
-    PrintStream sout;
-    UserInput(PrintStream newsout){sout = newsout;}
-    public void run(){
-        Scanner myObj = new Scanner(System.in);
-        
-        JPanel jp = new JPanel();
-        jp.setSize(500,100);
-        JavaFinalProjectPlayer.jf.add(jp, BorderLayout.SOUTH);
-        
-        JTextField text = new JTextField(20);
-        jp.add(text);
-        
-        JButton jb = new JButton("SEND");
-        jb.addActionListener(new ButtonListener(text));
-        jp.add(jb);
-        
-        JavaFinalProjectPlayer.jf.setVisible(true);
-        
-    }
-    class ButtonListener implements ActionListener{
-        JTextField text;
-        ButtonListener(JTextField newtext){text = newtext;}
-        @Override
-        public void actionPerformed(ActionEvent arg0){
-            sout.println(text.getText());
-            text.setText("");
-        }
-    }
-}
-
-class GetMsg extends Thread{
-    Scanner sin;
-    GetMsg(Scanner newsin){sin = newsin;}
-    public void run(){
-        
-        JPanel jp = new JPanel();
-        jp.setSize(500,500);
-        JTextArea textArea = new JTextArea();
-        //jp.add(textArea);
-        
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        textArea.setEditable(false);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPane.setPreferredSize(new Dimension(500, 500));
-        
-        jp.add(scrollPane);
-        
-        JavaFinalProjectPlayer.jf.add(jp);
-        
-        JavaFinalProjectPlayer.jf.setVisible(true);
-        while(true){
-            String msg = sin.nextLine();
-            textArea.append(msg+'\n');
-            textArea.setCaretPosition(textArea.getDocument().getLength());
-            JavaFinalProjectPlayer.jf.setVisible(true);
-        }
-    }
+   
+class StartAgain implements ActionListener{
+    StartAgain(){}
     
+    @Override
+        public void actionPerformed(ActionEvent arg0){
+            
+        }
+        
+    void won(){ //display won message to client 
+        JLabel label = new JLabel("You Won!!!", SwingConstants.CENTER);
+        label.setFont(new Font("Serif", Font.BOLD, 20));
+        label.setForeground(Color.RED);
+        label.setBackground(Color.ORANGE);
+        label.setOpaque(true);
+
+        //jf.add(label); 
+        //frame.setSize(300,150);
+        //jf.setVisible(true);
+    }
+    void lose(){ //display lsot message to client
+        JLabel label = new JLabel("You Lost...", SwingConstants.CENTER);
+        label.setFont(new Font("Serif", Font.BOLD, 20));
+        label.setForeground(Color.RED);
+        label.setBackground(Color.ORANGE);
+        label.setOpaque(true);
+
+        //jf.add(label); 
+        //frame.setSize(300,150);
+        //jf.setVisible(true);
+    }
+    void tie(){
+        //text.setText("");
+        // reset buttons/bunnies
+    }
 }
+       
